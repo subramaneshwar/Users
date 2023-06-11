0dfa4b5a42db40f91becb1aee78be98327c137a1
subramaneshwar <subramaneshwar@gmail.com>
2023-06-11 16:32:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import User from './User';
+import Navbar from './NavBar';
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar/>
+    <User/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/NavBar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Nav.css'
+function Navbar() {
+
+  return (
+    
+    <div className='nav'>
+        <li>
+
+
+            Home
+        </li>
+        <li>
+
+            Products
+        </li>
+        <li>
+
+            Users
+        </li>
+        <li>
+
+            Contact
+        </li>
+    </div>
+
+  )
+}
+
+export default Navbar
